a477b61938bd67d12734a59c9bd8892134d0717f
ratnesh0203 <118505626+ratnesh0203@users.noreply.github.com>
2024-02-22 19:42:31 +0000

    container config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 433374
+let accountEmail = "ratnesh@gmail.com"
+var accountPassword = "Ratnesh@123"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 36754
+accountEmail = "Ratnesh@google.com"
+accountPassword = "Rahul@123"
+accountCity = "Hyderabad"
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+/*
+prefer not to use var
+bcz of issue in block socpe and functionnal scope
+*/
