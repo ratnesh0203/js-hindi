39ef0ce3e4a0fd4e9b62ee4778cba3fa65af38a7
ratnesh0203 <118505626+ratnesh0203@users.noreply.github.com>
2024-02-26 18:26:31 +0000

    container config done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,16 @@
+// "use strict"; //  treat all js code as newer version
+// alert ( 3 + 3) // we are using node js not browser
+
+
+// dataypes 
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value 
+// undefined => value whis is only declared
+// ex let state;
+
+// object
+console.log(typeof undefined ); // undefined type
+console.log(typeof null); //object type
