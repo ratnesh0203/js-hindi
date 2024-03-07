dd7a936385e571eddb987ca5041e09a17c98eacc
ratnesh0203 <118505626+ratnesh0203@users.noreply.github.com>
2024-03-07 14:02:53 +0000

    container config done

---
+++ 01_Basics/conversionOperations.js
@@ -0,0 +1,22 @@
+// let score = "33";//it print 33 and type is number i.e. 33
+//let score = null; //in this case it print 0 and type is object
+// let score = undefined;//it will print not a number and type is string
+// let score = true; //it means type is boolean and it print 1 for true and 0 for false
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+ 
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+ 
+// 1=>true; 0=>false
+// "" => false
+// "Hitesh"=> true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
